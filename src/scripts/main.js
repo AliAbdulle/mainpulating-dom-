0223f873d84a9379765d2b6f7e8e79a3f166da6c
AliAbdulle <alidhobey@gmail.com>
2019-02-27 11:34:10 -0600

    Worked after

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,13 @@ console.log("Linked main.js");
 
 const sectionEl =document.querySelector(".article__section")
 console.log("Contents of  vairable sectionEL, sectionEL")
+
+const footerEl = document.querySelector(".article__footer")
+console.log(footerEl)
+
+
+const smallTextEl = document.querySelector(".smalText")
+console.log("Befor changing the console", smallTextEl)
+smallTextEl.classList.remove("smallText")
+smallTextEl.classList.add("largeText")
+console.log("After changing the console", smallTextEl)
